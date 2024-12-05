b214ef3568c42199349142c74dca84f7cd6db69d
Rupesh Mishra <64422429+Rupesh-1901@users.noreply.github.com>
2024-12-06 00:55:07 +0530

    git commit in root

--- 3am-web-akhilesh-dev/client/src/components/FormComponent/TextError.jsx
+++
@@ -1,9 +0,0 @@
-export const TextError = (props) => {
-  return (
-    <div className="form-error" style={props.styles}>
-      <div style={{ height: "14px", width: "15px", color: "red" }}>
-        {props.children}
-      </div>
-    </div>
-  );
-};
